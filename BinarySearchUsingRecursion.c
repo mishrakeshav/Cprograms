0d8f6b4b28975125ae303439c938183893d457e6
Keshav Mishra <47028747+mishrakeshav@users.noreply.github.com>
2019-04-23 10:58:22 +0530

    Add files via upload

---
+++ BinarySearchUsingRecursion.c
@@ -0,0 +1,28 @@
+// binary search...
+#include<stdio.h>
+int b_search(int a[], int s, int start, int end);
+int main()
+{
+    int a[20], n, search, i;
+    printf("Enter number of elements of the array  order: \n");
+    scanf("%d", &n);
+    printf("\nEnter Elements : \n");
+    for(i=0; i<n; i++)
+        scanf("%d", &a[i]);
+    printf("\nEnter number to be search : ");
+    scanf("%d", &search);
+    int k= b_search(a, search, 0, n-1);
+    if(k==-1)
+        printf("\nValue not found!");
+    else
+        printf("\nValue found at index %d", k);
+    return 0;
+}
+int b_search(int a[], int s, int start , int end)
+{
+   if(s>end)
+    return -1;
+   int mid= (start+ end)/2;
+
+   return(s==a[mid]? mid:s<a[mid]? b_search(a, s, start, mid-1 ): b_search(a, s, mid+1, end ));
+}
